f4a06d3d53a2b74b17455ce607e03a4ca524cab1
Maryam Chaudhry <maryamc1798@gmail.com>
2021-03-02 13:46:54 -0500

    working on saving books

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,1 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import API from "../utils/API";
+
+class Saved extends Component {
+
+    state = {
+        savedBooks: []
+    }
+
+    componentDidMount() {
+        API.savedBooks().then(savedBooks => this.setState({ savedBooks: savedBooks })).catch(err => console.error(err));
+    };
+
+    render(){
+        return(
+            <div className="container">
+                <h2>Saved Books</h2>
+
+            </div>
+        )
+    }
+}
+
+export default Saved;
