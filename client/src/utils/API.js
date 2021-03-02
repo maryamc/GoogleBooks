123b16070661213a1d18693db99ba96b17cc9e76
Maryam Chaudhry <maryamc1798@gmail.com>
2021-03-01 21:07:33 -0500

    updating api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,30 @@
 import axios from "axios";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get('https://www.googleapis.com/books/v1/volumes?q=${query}');
+  //Gets all books
+  getBooks: function (query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
+
+  //Delets books with id
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
   },
+
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
   }
 };
+
+
+
+  
+  // Gets the book with the given id
+  // getBook: function(id) {
+    // return axios.get("/api/books/" + id);
+  // },
+  
+  
+
